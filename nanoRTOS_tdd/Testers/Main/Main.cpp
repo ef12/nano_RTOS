839887a58e494510eefbf4d440bf0289b75a8863
Ehud <ehud@ridedott.com>
2019-11-25 15:18:44 +0100

    Add a makefile project for the tdd of the nanoRTOS

---
+++ nanoRTOS_tdd/Testers/Main/Main.cpp
@@ -0,0 +1,16 @@
+/*
+ * Main.cpp
+ *
+ *  Created on: 18 Nov 2019
+ *      Author:
+ */
+
+#include <iostream>
+#include "CppUTest/CommandLineTestRunner.h"
+#include "CppUTest/TestHarness.h"
+
+
+int main(int ac, char** av)
+{
+    return RUN_ALL_TESTS(ac, av);
+}
